bbe5170958a60882197b3b7cf31eb0a045960fa3
raviraj <ravirajsingh@ravirajs-air.localdomain>
2024-01-27 09:16:19 +0100

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id ("org.springframework.boot") version ("3.1.3")
+    id ("io.spring.dependency-management") version ("1.0.11.RELEASE")
+    id ("java")
+}
+
+group = "com.imc"
+version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation ("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("junit:junit:4.13.1")
+
+    compileOnly ("org.projectlombok:lombok:1.18.30")
+    annotationProcessor ("org.projectlombok:lombok:1.18.30")
+    testCompileOnly ("org.projectlombok:lombok:1.18.30")
+    testAnnotationProcessor ("org.projectlombok:lombok:1.18.30")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.mockito:mockito-core:2.1.0")
+    testImplementation ("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
