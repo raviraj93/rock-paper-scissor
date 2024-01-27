669cf628b662059963e8aa75328f5364a3dea06e
raviraj <ravirajsingh@ravirajs-air.localdomain>
2024-01-27 18:01:59 +0100

    update readme

--- src/main/java/com/imc/rockpaperscissor/RockPaperScissorsApplication.java
+++ src/main/java/com/imc/rockpaperscissor/RockPaperScissorsApplication.java
@@ -1,15 +1,14 @@
 package com.imc.rockpaperscissor;
 
-import com.imc.rockpaperscissor.wrapper.ScannerWrapper;
 import com.imc.rockpaperscissor.domain.Player;
 import com.imc.rockpaperscissor.factory.PlayerFactory;
 import com.imc.rockpaperscissor.service.GameService;
+import com.imc.rockpaperscissor.wrapper.ScannerWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class RockPaperScissorsApplication implements CommandLineRunner {
